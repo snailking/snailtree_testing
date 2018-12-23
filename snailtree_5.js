7f843e4b64e1179938385bd8544afe105c890680
snailking <39387494+snailking@users.noreply.github.com>
2018-12-23 18:27:21 +0100

    Add files via upload

--- snailtree_5.js
+++ snailtree_5.js
@@ -164,7 +164,7 @@ function timeSincePlayerClaim(){
 //Fill up the field with player pecans
 function maxField(){
 	f_pecan = a_playerPecan;
-	document.getElementById('fieldPecan').innerHTML = a_playerPecan;
+	document.getElementById('fieldPecan').value = a_playerPecan;
 }
 
 /* UPDATE */
